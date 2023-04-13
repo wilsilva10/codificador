08e6b232b928dddb4ac2568fd9f1cc06786e30a4
Wilber Silva <wilberoliveira1@hotmail.com>
2023-04-13 08:45:31 -0300

    código do desafio

---
+++ js/padrao.js
@@ -0,0 +1,7 @@
+export const patterns = [
+    {in: 'a', out: 'ai'},
+    {in:'e', out: 'enter'},
+    {in: 'i', out: 'imes'},
+    {in: 'o', out: 'ober'},
+    {in: 'u', out: 'ufat'},
+]
---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+import { decrypt} from "./decrypt.js";
+import { encrypt } from "./encrypt.js";
+
+
+const btnEncrypt = document.querySelector("#btn-encrypt");
+const btnDecrypt = document.querySelector("#btn-decrypt");
+const btnCopy = document.querySelector("#btn-copy");
+
+const textInput = document.querySelector(".textarea");
+
+const resultContainer = document.querySelector(".result");
+const noResultContainer = document.querySelector(".no-result");
+
+function getResultText() {
+  const resultTag = document.querySelector(".result__text");
+  return resultTag.innerText;
+}
+
+function hideResultContainer() {
+  resultContainer.classList.remove("active");
+}
+
+function showResultContainer() {
+  resultContainer.classList.add("active");
+}
+
+function hideNoResultContainer() {
+  noResultContainer.classList.add("inative");
+}
+
+function showNoResultContainer() {
+  noResultContainer.classList.remove("inative");
+}
+
+function setResult(result) {
+  const resultTag = document.querySelector(".result__text");
+  resultTag.innerText = result;
+}
+
+function setTextInputValue(result) {
+  textInput.innerText = result;
+}
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text);
+}
+
+function copyTextToClipboard() {
+  copyToClipboard(getResultText());
+  alert("O texto foi copiado!");
+}
+
+function onEncrypt() {
+  if (textInput.value.trim()) {
+    const encrypted = encrypt(normalizeString(textInput.value));
+    hideNoResultContainer();
+    showResultContainer();
+    setResult(encrypted);
+    setTextInputValue("");
+  } else {
+    showNoResultContainer();
+    hideResultContainer();
+  }
+}
+
+function normalizeString(str) {
+  return str.normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+}
+
+function onDecrypt() {
+  if (textInput.value.trim()) {
+    const decrypted = decrypt(normalizeString(textInput.value));
+    hideNoResultContainer();
+    showResultContainer();
+    setResult(decrypted);
+    setTextInputValue("");
+  } else {
+    showNoResultContainer();
+    hideResultContainer();
+  }
+}
+
+btnEncrypt.addEventListener("click", onEncrypt);
+
+btnDecrypt.addEventListener("click", onDecrypt);
+
+btnCopy.addEventListener("click", copyTextToClipboard);
